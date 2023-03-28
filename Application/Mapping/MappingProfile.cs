4b99b5b6ca6411933a489e0d7ef49ea51f421bb4
Khanh Nguyen <khanh.nguyen@sympli.com.au>
2023-03-28 18:24:09 +0700

    Add multiple sources mapping capabilities

--- Application/Mapping/MappingProfile.cs
+++ Application/Mapping/MappingProfile.cs
@@ -15,7 +15,12 @@ public interface IMapping<TSource, TDest>
         else
             extraMappingConfig.Invoke(profile.CreateMap<TSource, TDest>());
     }
-
+    
+    /// <summary>
+    /// Personally, I don't see Reverse mapping is a good way if we wish reverse mapping direction.
+    ///     There's no logic to guarantee the custom mapping if any reverse correctly.
+    /// We can consider whether to have this or not.
+    /// </summary>
     void MappingTo(Profile profile)
     {
         var extraMappingConfig = ExtraMappingToConfiguration();
@@ -24,6 +29,11 @@ public interface IMapping<TSource, TDest>
         else
             extraMappingConfig.Invoke(profile.CreateMap<TDest, TSource>());
     }
+}
+
+public interface IMultipleSourceMapping
+{
+    void Mapping(Profile profile);
 }
 
 public class MappingProfile : Profile
@@ -36,11 +46,11 @@ public class MappingProfile : Profile
     
     private void ApplyMappingsFromAssembly(Assembly assembly)
     {
-        var types = assembly.GetExportedTypes()
+        var standardPairMappingTypes = assembly.GetExportedTypes()
             .Where(t => t.GetInterfaces().Any(i =>
                 i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapping<,>)));
 
-        foreach (var type in types)
+        foreach (var type in standardPairMappingTypes)
         {
             var mapInterface = type.GetInterfaces().First(x => x.Name.StartsWith(nameof(IMapping<object, object>)));
             var mapType = typeof(IMapping<,>).MakeGenericType(mapInterface.GetGenericArguments());
@@ -49,16 +59,29 @@ public class MappingProfile : Profile
             mapType.GetMethod(nameof(IMapping<object, object>.MappingFrom))?.Invoke(instance, new object[] { this });
             mapType.GetMethod(nameof(IMapping<object, object>.MappingTo))?.Invoke(instance, new object[] { this });
         }
+        
+        var multipleSourceMappingTypes = assembly.GetExportedTypes()
+            .Where(t => t.GetInterfaces().Any(i =>
+                i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMultipleSourceMapping)));
+
+        foreach (var type in multipleSourceMappingTypes)
+        {
+            var instance = Activator.CreateInstance(type);
+            var methodInfo = type.GetMethod(nameof(IMultipleSourceMapping.Mapping));
+            methodInfo?.Invoke(instance, new object[] { this });
+        }
     }
 
-    /*
-     * Each mapping pair are unique and shouldn't be re-use
-     * Mapping should always implement from highest layer to lowest (source type shouldn't care what destination type
-     *  it is going to be mapped)
-     * In case multiple sources wish to map to a single destination type, define them here.
-     */
+    /// <summary>
+    /// Each mapping pair are unique and shouldn't be re-use
+    /// Mapping should always implement from highest layer to lowest (source type shouldn't care what destination type
+    ///     it is going to be mapped)
+    /// In case multiple sources wish to map to a single destination type, define them here.
+    ///     This case should not happen frequently by design, either re-use destination models
+    ///     or create other destination models for the specific job if needed.
+    /// Or in my PoV, we make use of IMultipleSourceMapping interface above is better.
+    /// </summary>
     void SingleDestinationMappingWithMultipleSources()
     {
-        
     }
 }
--- Application/Models/ProviderDto.cs
+++ Application/Models/ProviderDto.cs
@@ -4,7 +4,7 @@ using Domain.Models;
 
 namespace Application.Models;
 
-public class ProviderDto : IMapping<Provider, ProviderDto>
+public class ProviderDto : IMultipleSourceMapping
 {
     public Guid Id { get; set; }
     public string Name { get; set; }
@@ -12,6 +12,14 @@ public class ProviderDto : IMapping<Provider, ProviderDto>
     public string PhoneNumber { get; set; }
     public bool IsOffshoreProvider { get; set; }
 
-    public Action<IMappingExpression<Provider, ProviderDto>> ExtraMappingFromConfiguration() => config =>
-        config.ForMember(x => x.IsOffshoreProvider, opt => opt.MapFrom(src => src.State != "NSW"));
+    /// <summary>
+    /// Mapping multiple sources here. Instead of using SingleDestinationMappingWithMultipleSources
+    /// in MappingProfile.
+    /// </summary>
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Provider, ProviderDto>()
+            .ForMember(x => x.IsOffshoreProvider, opt => opt.MapFrom(src => src.State != "NSW"));
+        profile.CreateMap<Organization, ProviderDto>();
+    }
 }

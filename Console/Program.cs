62899b800640f328c829ccfe04d440be23cf1569
Khanh Nguyen <khanh.nguyen@sympli.com.au>
2023-03-28 14:06:09 +0700

    Initial commit

---
+++ Console/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+var serviceProvider = services.BuildServiceProvider();
+
+using var scope = serviceProvider.CreateScope();
+Console.WriteLine("Hello World!");
